b0d82b9c6b70808ba19bc1dc15fc42620c59dd48
LYC <929194891@qq.com>
2017-08-18 14:21:04 +0800

    增加注解事务

--- app/src/main/java/com/hqg/service/impl/UserServiceImpl.java
+++ app/src/main/java/com/hqg/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.hqg.service.impl;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hqg.dao.UserDao;
 import com.hqg.entity.User;
 import com.hqg.service.UserService;
 
+@Transactional
 @Service("UserService")
 public class UserServiceImpl implements UserService{
 
